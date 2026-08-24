b81d4b42fec465c515ac2819cf3f98f3a75346d7
Lauri Lam <laurilam360@gmail.com>
2026-08-24 11:55:17 +0300

    Taks-Manager 1.1

--- task-manager/frontend/src/components/TaskFilter.tsx
+++ task-manager/frontend/src/components/TaskFilter.tsx
@@ -2,18 +2,35 @@ export type FilterType = "all" | "active" | "completed";
 
 type TaskFilterProps = {
     onFilterChange: (filter: FilterType) => void;
+    filter: FilterType
 }
 
-const TaskFilter = ({ onFilterChange }: TaskFilterProps) => {
+const TaskFilter = ({ onFilterChange, filter }: TaskFilterProps) => {
     return (
         <>
-            <button onClick={() => onFilterChange("all")} className="btn btn-outline-secondary">
+            <button
+                onClick={() => onFilterChange("all")}
+                className={filter === "all" ?
+                    "btn btn-secondary" :
+                    "btn btn-outline-secondary"
+                }>
                 All
             </button>
-            <button onClick={() => onFilterChange("active")} className="btn btn-outline-secondary">
+            <button
+                onClick={() => onFilterChange("active")}
+                className={filter === "active" ?
+                    "btn btn-secondary" :
+                    "btn btn-outline-secondary"
+                }
+                >
                 Active
             </button>
-            <button onClick={() => onFilterChange("completed")} className="btn btn-outline-secondary">
+            <button
+                onClick={() => onFilterChange("completed")}
+                className={filter === "completed" ?
+                    "btn btn-secondary" :
+                    "btn btn-outline-secondary"
+                }>
                 Completed
             </button>
         </>
